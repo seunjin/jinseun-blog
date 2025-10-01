2cf25b9b50f1db4d3e9943b5e9e337b321027945
seunjin <jinseun@gmail.com>
2025-10-01 16:09:20 +0900

    refactor: move query client provider to src/providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AppQueryClientProvider } from "./providers/query-client-provider";
+import { AppQueryClientProvider } from "@/providers/query-client-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
